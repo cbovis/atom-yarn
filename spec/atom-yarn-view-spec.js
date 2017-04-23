7a400abcd056fccaf02bbb35321125fe33e74b97
Craig Bovis <craig.bovis@inlight.com.au>
2017-04-24 00:44:05 +1000

    Initial commit

---
+++ spec/atom-yarn-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomYarnView from '../lib/atom-yarn-view';
+
+describe('AtomYarnView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
