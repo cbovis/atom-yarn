33613646d81d3038ed01980ef941b0085cbc6b12
Craig Bovis <craig.bovis@inlight.com.au>
2017-06-10 15:33:23 +1000

    Add linting rules for classes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
   extends: "airbnb-base",
-  plugins: ["import"],
-  globals: { atom: true },
+  plugins: ["import", "classes"],
+  globals: { atom: true, document: true },
   rules: {
-    "no-console": "off"
+    "no-console": "off",
+    "classes/style": 2,
+    "classes/super": 2,
+    "classes/name": [2, "class", "method"],
+    "classes/space": 2
   },
   settings: {
     "import/core-modules": ["atom"]
